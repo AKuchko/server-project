80fb705c99973a723f41eeebce6468fab8e21b81
AK <101558236+AKuchko@users.noreply.github.com>
2023-08-03 20:49:50 +0700

    add: Group model, group factory

--- models/index.js
+++ models/index.js
@@ -5,16 +5,19 @@ mongoose.Promise = Promise;
 const settingsConnection = mongoose.createConnection(config.get('mongodb'));
 
 const {serverSchema} = require('./server');
-const {userActionSchema} = require('./user-action')
-const {taskSchema} = require('./task')
+const {userActionSchema} = require('./user-action');
+const {taskSchema} = require('./task');
+const {groupSchema} = require('./group');
 
 const Server = settingsConnection.model('servers', serverSchema);
 const UserAction = settingsConnection.model('useractions', userActionSchema);
 const Task = settingsConnection.model('tasks', taskSchema);
+const Group = settingsConnection.model('group', groupSchema);
 
 
 module.exports = {
   Server,
   UserAction,
   Task,
+  Group,
 };
